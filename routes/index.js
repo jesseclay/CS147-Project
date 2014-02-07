b1ba5bcb7a687e71a4bc0ca250c42fe6b4f18e00
Timothy Santos <tsantos@stanford.edu>
2014-02-07 00:39:03 +0000

    added link to homescreen

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,5 @@
 // Get all of our friend data
-var data = require('../data.json');
-
 exports.view = function(req, res){
-	console.log(data);
-	var filtered_friends = data.friends;
 	res.render('index');
 };
 
