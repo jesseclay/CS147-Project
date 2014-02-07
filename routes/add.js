b45491eac9d6859d067346d45c4e40cd60902dd9
Timothy Santos <tsantos@stanford.edu>
2014-02-07 00:18:07 +0000

    cleaned up template

--- routes/add.js
+++
@@ -1,14 +0,0 @@
-var data = require('../data.json');
-
-exports.addFriend = function(req, res) {    
-	var filtered_friends = data.friends;
-	var name = req.query.name;
-	var description = req.query.name;
-	var newFriend = {"name": name,
-			"description": description,
-			"imageURL": "http://lorempixel.com/400/400/people"};
-			console.log(newFriend);
-	data["friends"].push(newFriend);
-	res.render('add', newFriend);
-	
- };
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var data = require('../data.json');
 exports.view = function(req, res){
 	console.log(data);
 	var filtered_friends = data.friends;
-	res.render('index', { 'friend': filtered_friends });
+	res.render('index');
 };
 
 
