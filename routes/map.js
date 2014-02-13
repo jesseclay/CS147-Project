fc3e8f607b61e65592f7f4b211055238e32bbf47
vagrant <vagrant@precise32.(none)>
2014-02-13 06:44:50 +0000

    added list of groups

--- routes/map.js
+++ routes/map.js
@@ -1,5 +1,8 @@
+var data = require('../groupData.json');
+
 exports.view = function(req, res){
 	res.render('map', {
-		'title' : req.query.name
+		'title' : req.query.name,
+		'data' : data
 	});
 };
