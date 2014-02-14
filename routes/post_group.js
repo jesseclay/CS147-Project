06832f3ee09ce933a2b9f1a8c9fccc24a630b40e
vagrant <vagrant@precise32.(none)>
2014-02-14 07:56:58 +0000

    redirected addgroups to map page

--- routes/post_group.js
+++ routes/post_group.js
@@ -1,18 +1,23 @@
 var data = require('../groupData.json');
 
 exports.view = function(req, res){
-	res.render('post_group');
+	res.render('post_group',{
+		"title": req.query.classname
+	});
 };
 
 exports.add = function(req,res){
 	var newgroup = 
 		{
-			"name": req.query.name,
+			"name": req.query.groupname,
 			"location": req.query.location,
 			"id": "newgroup",
 			"startTime": "6:00 PM",
 			"endTime": "8:00 PM"
-		}
+		};
 	data["groups"].unshift(newgroup);
-	res.render('home');
+	res.render('map', {
+		'title' : req.query.classname,
+		'data' : data
+	});
 };
