b3832470ea2f666dd24b898bd5ee05f88ef75548
vagrant <vagrant@precise32.(none)>
2014-02-21 04:51:31 +0000

    Added to map

--- public/js/mapEvents.js
+++ public/js/mapEvents.js
@@ -13,21 +13,24 @@ function initializePage() {
 	$(".list-group-item").unbind("click").bind("click", function(e){
 		e.preventDefault();
 		var valueElement = $(this).find(".valueContainer");
-		if (valueElement.css("visibility")=="hidden"){
+		if (valueElement.css("display")=="none"){
 			$(this).find('.extraInfo').css("display","none");
-			valueElement.css("visibility","visible");
+			valueElement.css("display","inline-block");
+			$(this).find('.nameContainer').css('width',($(this).width()-valueElement.width()-20)+"px");
 		}
 		else if($(this).find('.extraInfo').size()!=0){
 			$(this).find('.extraInfo').css("display","default");
-			valueElement.css("visibility","hidden");
+			valueElement.css("display","none");
+			$(this).find('.nameContainer').css('width',$(this).width()-20+"px");
 		}
 		else{
 			var group = $(this).data();
 			var infoHTML = "<div class='extraInfo'>Time: " + group.starttime + " - " + 
 			group.endtime + "</div>" +  
 			"<div class='extraInfo'>Location: " + group.location + "</div>";
-			valueElement.css("visibility","hidden");
+			valueElement.css("display","none");
 			$(this).append(infoHTML);
+			$(this).find('.nameContainer').css('width',$(this).width()-20+"px");
 		}
 	});
 }
