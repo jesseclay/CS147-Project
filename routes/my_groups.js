565215460928637f9cba9a74480a324cc357bc17
vagrant <tle16@stanford.edu>
2014-03-12 01:00:32 +0000

    modified mygroups

--- routes/my_groups.js
+++ routes/my_groups.js
@@ -5,16 +5,56 @@ exports.view = function(req, res){
 	if(userid === undefined) {
     	res.render('index');
     }
-	var db = require("../db")
+	var db = require("../db");
+    
+    var hasGroups = false;
     db.getMyGroups(function (groups) {
     	if(groups) {
     		console.log("group info: " + groups);
+            if(groups.length > 0) {
+                hasGroups = true;
+            }
+            groups = replaceTimes(groups);
 			res.render('my_groups', {
-			'data' : groups
+                'data' : groups,
+                'hasGroups' : hasGroups
 			});
         }
     }, userid);
-
-    
-  
 };
+
+function replaceTimes(groups) {
+    for (var i = 0; i < groups.length; i++) {
+        var group = groups[i];
+        var dateObjectStart = new Date(Date.parse(group.startTime));
+        var startTime = putInTimeFormat(dateObjectStart);
+        var dateObjectEnd = new Date(Date.parse(group.endTime));
+        var endTime = putInTimeFormat(dateObjectEnd);
+        group.startTime = startTime;
+        group.endTime = endTime;
+    }
+    return groups;
+}
+
+
+function putInTimeFormat(dateObject) {
+    var hour = parseInt(dateObject.getHours());
+    var minutes = dateObject.getMinutes().toString();
+    var time = "";
+    var ampm = "am";
+    if(hour >= 12) {
+        ampm = "pm";
+        if(hour !== 12) {
+            hour = hour - 12;
+        }
+    }
+    if(hour === 0) {
+        hour = 12;
+    }
+    if(minutes < 10) {
+        time = hour + ":" + minutes + "0" + ampm;
+    } else {
+        time = hour + ":" + minutes + ampm;
+    }
+    return time;
+}
