2fad5cd82a0f2d7ac7d48b1ff76118e57a97fe50
Timothy Santos <tsantos@stanford.edu>
2014-03-13 21:44:59 +0000

    added straight to home screen if already logged in

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 exports.view = function(req, res){
-	res.render('index');
+	var userid = req.session.userid;
+	if(userid !== undefined) {
+    	res.render('home');
+    } else {
+		res.render('index');
+	}
 };
 
 
